921c07fdf98700529c437348e35feb5ccb23ee7e
Ethena lee <yeonji.900111@gmail.com>
2018-11-29 19:37:43 +0000

    loadedDie done

---
+++ loadedDie.js
@@ -0,0 +1,30 @@
+function makeIdGenerator() {
+  var id = 0;
+
+  // The following is the closure function
+  return function() {
+    // This inner function accesses and assigns the value of
+    // the variable id, which was defined in the parent function
+    id += 1;
+    return id;
+  }
+}
+
+var nextId = makeIdGenerator();
+
+
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+
+  return function() {
+    return list[nextId()-1];
+  }
+}
+
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
