b42e0158fa02a74f2b0e515d0187d3b7d47b688d
Ethena lee <yeonji.900111@gmail.com>
2018-11-29 20:24:03 +0000

    count done

---
+++ count.js
@@ -0,0 +1,31 @@
+var countdownGenerator = function (x) {
+  /* your code here */
+
+      return function() {
+        if(x === 0) {
+          console.log("Blast off!");
+          x -= 1;
+        }
+        else if(x < 0) {
+          console.log("Rockets already gone, bub!");
+        }
+        else {
+          console.log("T-minus", x);
+          x -= 1;
+        }
+      }
+
+
+};
+
+
+
+
+
+var countdown = countdownGenerator(3);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
